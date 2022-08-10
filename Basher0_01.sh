00c21fe7df3f5a458ecf27bbd220800a7b356ce4
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-10 00:09:27 -0700

    Update Basher0_01.sh

--- Basher0_01.sh
+++ Basher0_01.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-ls -l
+ls -l #Get Directory at current level : ll does not appear to work
 echo -e '--------------------'
 java --version
 echo -e '--------------------'
 python3 --version
 echo -e '--------------------'
-hostname -I
+hostname -I #Get host IP
 echo -e '--------------------'
-hostnamectl
+hostnamectl #Get system data
 echo -e '--------------------'
 date
 echo -e '--------------------'
-vmstat 2
+vmstat 2 #get current processes : Updates every 2 sec
 echo -e '--------------------'
