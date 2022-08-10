41e41156abc40afc92e7ff24276d4c7dcfd90ad5
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-09 23:19:49 -0700

    Create Basher0_01.sh

---
+++ Basher0_01.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ls -l
+echo -e '--------------------'
+java --version
+echo -e '--------------------'
+python3 --version
+echo -e '--------------------'
+hostname -I
+echo -e '--------------------'
+hostnamectl
+echo -e '--------------------'
+date
+echo -e '--------------------'
+vmstat 2
+echo -e '--------------------'
